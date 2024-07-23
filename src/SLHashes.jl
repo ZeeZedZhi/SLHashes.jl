87e202939ba791dd3d82bdfd063f1f3976becf2e
zeezedzhi <zhi.liu.lin@outlook.com>
2024-07-23 19:41:36 -0400

    reformatted the function signature for get_slhash and added a prime number to restrict the codomain to a finite field

--- src/SLHashes.jl
+++ src/SLHashes.jl
@@ -4,25 +4,41 @@ using LinearAlgebra: Tridiagonal, I
 export get_slhash
 
 
-function get_slhash(first_lambda::Int, n::Int, a::Int, b::Int, l::Int, A_map::Vector{Int}, Ainv_map::Vector{Int}, B_map::Vector{Int}, Binv_map::Vector{Int})
+function get_slhash(first_lambda::Int, n::Int, a::Int, b::Int, l::Int, mappings::Matrix{Int}, p::Int=0)
 	A::Matrix{Int} = Tridiagonal(zeros(n-1), ones(n), a*ones(n-1))^l
 	B::Matrix{Int} = Tridiagonal(b*ones(n-1), ones(n), zeros(n-1))^l
 	Ainv::Matrix{Int} = A^-1
 	Binv::Matrix{Int} = B^-1
 	matrices = (A, B, Ainv, Binv)
 
-	mappings = (Tuple(A_map), Tuple(B_map), Tuple(Ainv_map), Tuple(Binv_map))
+	if p > 0
+		function slhashp(sequence::Vector)::Matrix{Int}
+			lambda = first_lambda
+			result = Matrix{Int}(I, n, n)
 
-	function slhash(sequence::Vector)::Matrix{Int}
-		lambda = first_lambda
-		result = Matrix{Int}(I, n, n)
-
-		for x in sequence
-			choice = mappings[lambda][x]
-			result *= matrices[choice]
-			lambda = (choice + 2) % 4
+			for x in sequence
+				choice = mappings[lambda, x]
+				result *= matrices[choice] .% p
+				lambda = (choice + 2) % 4
+			end
+			return result
 		end
-		return result
+
+		return slhashp
+	else
+		function slhash(sequence::Vector)::Matrix{Int}
+			lambda = first_lambda
+			result = Matrix{Int}(I, n, n)
+
+			for x in sequence
+				choice = mappings[lambda, x]
+				result *= matrices[choice]
+				lambda = (choice + 2) % 4
+			end
+			return result
+		end
+
+		return slhash
 	end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using SLHashes
 using Test
 
 @testset "SLHashes.jl" begin
-    # Write your tests here.
+    
+    sequence = [2, 2, 3, 2, 2, 2, 1]
+	testhash1 = get_slhash(first_lambda=1, n=3, a=4, b=2, l=4, mappings=[2 3 4; 1 3 4; 1 4 2; 1 3 2])
+	@test testhash1(sequence) = [694190977 233260720 29297952; -38379648 -12896255 -1619792; 1191936 400512 50305]
+
 end
