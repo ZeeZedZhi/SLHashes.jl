1b8f04ba9c9a9304ad2f63240f784d19372ead16
Zhi Lin <zhi.liu.lin@outlook.com>
2024-07-23 20:15:25 -0400

    wrote documentation for get_slhash and wrote some tests

--- src/SLHashes.jl
+++ src/SLHashes.jl
@@ -4,21 +4,41 @@ using LinearAlgebra: Tridiagonal, I
 export get_slhash
 
 
+"""
+    get_slhash(first_lambda::Int, n::Int, a::Int, b::Int, l::Int, mappings::Matrix{Int}, p::Int=0)
+
+Returns a hash function that maps sequences with terms in {1, 2, 3} to `n` by `n` matrices over the finite field of order `p`. nonpositive `p` result in matrices over all integers. 
+
+Use integers to reference matrices; specifically:
+	``1`` for ``A``,
+	``2`` for ``B``,
+	``3`` for ``A^-1``, and
+	``4`` for ``B^-1``.
+
+Definitions of `n`, `a`, `b`, and `l` are given in the paper. ``first_lambda`` is the matrix that corresponds to the function that maps the first term.
+"""
 function get_slhash(first_lambda::Int, n::Int, a::Int, b::Int, l::Int, mappings::Matrix{Int}, p::Int=0)
 	A::Matrix{Int} = Tridiagonal(zeros(n-1), ones(n), a*ones(n-1))^l
 	B::Matrix{Int} = Tridiagonal(b*ones(n-1), ones(n), zeros(n-1))^l
 	Ainv::Matrix{Int} = A^-1
 	Binv::Matrix{Int} = B^-1
-	matrices = (A, B, Ainv, Binv)
 
 	if p > 0
+		A = mod.(A, p)
+		B = mod.(B, p)
+		Ainv = mod.(Ainv, p)
+		Binv = mod.(Binv, p)
+
+		matrices = (A, B, Ainv, Binv)
+
 		function slhashp(sequence::Vector)::Matrix{Int}
 			lambda = first_lambda
 			result = Matrix{Int}(I, n, n)
 
 			for x in sequence
 				choice = mappings[lambda, x]
-				result *= matrices[choice] .% p
+				result *= matrices[choice]
+				result = mod.(result, p)
 				lambda = (choice + 2) % 4
 			end
 			return result
@@ -26,6 +46,8 @@ function get_slhash(first_lambda::Int, n::Int, a::Int, b::Int, l::Int, mappings:
 
 		return slhashp
 	else
+		matrices = (A, B, Ainv, Binv)
+
 		function slhash(sequence::Vector)::Matrix{Int}
 			lambda = first_lambda
 			result = Matrix{Int}(I, n, n)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,14 @@ using Test
 @testset "SLHashes.jl" begin
     
     sequence = [2, 2, 3, 2, 2, 2, 1]
-	testhash1 = get_slhash(first_lambda=1, n=3, a=4, b=2, l=4, mappings=[2 3 4; 1 3 4; 1 4 2; 1 3 2])
-	@test testhash1(sequence) = [694190977 233260720 29297952; -38379648 -12896255 -1619792; 1191936 400512 50305]
+	testhash = get_slhash(1, 3, 4, 2, 4, [2 3 4; 1 3 4; 1 4 2; 1 3 2])
+	@test testhash(sequence) == [694190977 233260720 29297952; -38379648 -12896255 -1619792; 1191936 400512 50305]
+
+
+	primes = [17, 101, 257]
+	for prime in primes
+		testhashp = get_slhash(1, 3, 4, 2, 4, [2 3 4; 1 3 4; 1 4 2; 1 3 2], prime)
+		@test testhashp(sequence) == mod.([694190977 233260720 29297952; -38379648 -12896255 -1619792; 1191936 400512 50305], prime)
+	end
 
 end
